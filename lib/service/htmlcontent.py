658c37206f78d4865c70b2f5d7b17b2252f9b064
Johannes Findeisen <you@hanez.org>
2013-05-30 04:45:30 +0200

    added lib/service/ and some dummy files. need to talk about that but i think we should implement the services in pure python

---
+++ lib/service/htmlcontent.py
@@ -0,0 +1,8 @@
+"""
+The htmlcontent service in pure Python. STUPID NAME!!!
+"""
+
+# http://pycurl.sourceforge.net/ --- seems old...
+# http://www.angryobjects.com/2011/10/15/http-with-python-pycurl-by-example/
+#
+# maybe better: http://docs.python.org/2/library/urllib.html
---
+++ lib/service/ping.py
@@ -0,0 +1,5 @@
+"""
+The ping service in pure Python.
+"""
+
+# http://code.activestate.com/recipes/409689-icmplib-library-for-creating-and-reading-icmp-pack/
