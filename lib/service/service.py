a516b3dd1c4916a92bb0e9fe59100ac3443d9ef5
Johannes Findeisen <you@hanez.org>
2013-06-04 23:13:18 +0200

    added service class

---
+++ lib/service/service.py
@@ -0,0 +1,17 @@
+
+class Service:
+    def __init__(self, args):
+        self.args = args
+        pass
+
+    def execute(self):
+        pass
+
+    def pre_execute(self):
+        pass
+
+    def parse_result(self):
+        pass
+
+    def handle_result(self):
+        pass
