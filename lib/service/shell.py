b03d2893f539d02b5e98a2206bbffc07bf83add3
Johannes Findeisen <you@hanez.org>
2013-06-05 00:09:24 +0200

    added shell service and fixed service

--- lib/service/shell.py
+++ lib/service/shell.py
@@ -1,4 +1,18 @@
 """
-The shell service. This is for executing services as shell commands and don't use a builtin function. This is useful to
-be free to do what you want.
+The shell service. This is for executing services as shell commands
+and don't use a builtin function. This is useful to be free to do what you want.
 """
+
+from service import Service
+
+
+class ShellService(Service):
+    def __init__(self, parser, log, **kwargs):
+        super(Service, self).__init__(parser)
+        if "command" in kwargs:
+            self.command = kwargs["command"]
+        else:
+            log.w("There is no command")
+
+    def execute(self):
+        self.command.call()
