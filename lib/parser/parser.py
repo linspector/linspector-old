2676360cedc8e105c2e8eb1254fb7d700c161a35
Rafael.Timmerberg <rtimmerberg@eos-uptrade.com>
2013-06-04 23:06:05 +0200

    basic parser structure

---
+++ lib/parser/parser.py
@@ -0,0 +1,14 @@
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def parse_data(self, data):
+      self.pre_parse(data)
+      return self.generate_parse_result(data)
+  
+    def pre_parse(self, data):
+        pass
+    
+    def generate_parse_result(self, result):
+        pass
