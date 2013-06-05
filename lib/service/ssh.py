e9be20ecf0f02862947b88792f85b493ab86d861
Johannes Findeisen <you@hanez.org>
2013-06-05 04:20:26 +0200

    added some code to the service classes; no working code; mostly boilerplate...

--- lib/service/ssh.py
+++ lib/service/ssh.py
@@ -1,5 +1,43 @@
 """
-The ssh service in pure Python.
+The ssh service This is for executing remote shell commands and retrieve the output.
+
+This service is using paramiko (http://www.lag.net/paramiko/).
 """
 
-# http://www.lag.net/paramiko/
+import paramiko
+import pprint
+import os
+from service import Service
+
+
+class SshService(Service):
+    def __init__(self, parser, log, **kwargs):
+        super(Service, self).__init__(parser)
+        if "command" in kwargs:
+            self.command = kwargs["command"]
+        else:
+            log.w("There is no command")
+
+    def execute(self):
+        path = os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')
+        key = paramiko.RSAKey.from_private_key_file(path)
+
+        client = paramiko.SSHClient()
+        client.get_host_keys().add('hanez.org', 'ssh-rsa', key)
+        pprint.pprint(client._host_keys)
+
+        client.connect('hanez.org', username='hanez')
+
+        #self.command.call() ist dann das:
+        stdin, stdout, stderr = client.exec_command('ls')
+        for line in stdout:
+            print '... ' + line.strip('\n')
+        client.close()
+
+
+# def main():
+#     # service = SshService(parser, log, command='uptime')
+#     return
+#
+# if __name__ == "__main__":
+#     main()
