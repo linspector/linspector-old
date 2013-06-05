8bcac96942cc5628b51b5ad415568f1041aaade2
Johannes Findeisen <you@hanez.org>
2013-06-05 04:18:37 +0200

    just some config object cleanups

--- lib/config/config.py
+++ lib/config/config.py
@@ -1,11 +1,9 @@
 import json
-from services import serviceList
 from filters import parseFilterList
 from members import parseMemberList
-from hosts import parseHostList
 from periods import parsePeriodList
-from hostgroups import parseHostGroupList
-#from layouts import *
+#from hostgroups import parseHostGroupList
+#from layouts import parseLayoutList
 
 
 class Config:
@@ -17,21 +15,12 @@ class Config:
 
         self.dict = json.loads(self.config)
 
-        self.services = serviceList(self.dict['services'])
-
         self.filters = parseFilterList(self.dict['filters'])
-
         self.members = parseMemberList(self.dict['members'], self.filters, log)
-
-        self.periods = parsePeriodList(self.dict['periods'],log)
-
-        self.hosts = parseHostList(self.dict['hosts'], self.services, log)
-
-        self.hostgroups = parseHostGroupList(self.dict['hostgroups'],
-                                             self.hosts,
-                                             self.members,
-                                             self.periods,
-                                             self.services,
-                                             log)
+        self.periods = parsePeriodList(self.dict['periods'], log)
+        #self.hostgroups = parseHostGroupList(self.dict['hostgroups'],
+        #                                     self.members,
+        #                                     self.periods,
+        #                                     log)
 
         #self.layouts = LayoutList(self.dict['layouts'], self.hostgroups)
