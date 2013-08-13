90d59778a66ecdfd47fe0b24c23bd08ebfb77a89
Johannes Findeisen <you@hanez.org>
2013-08-13 23:10:06 +0200

    he surely is... ;)

---
+++ lib/frontends/https.py
@@ -0,0 +1,6 @@
+'''
+A HTTPS frontend to the current Linspector instance.
+
+A Webserver listening for requests to give information about the internal state of linspector.
+(maybe providing a JSON API to the instance too...)
+'''
