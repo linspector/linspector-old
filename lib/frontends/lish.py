cabf6a96a3ebd315f57b3ddb6f1156e6aad1e4a4
Johannes Findeisen <you@hanez.org>
2013-08-13 23:10:06 +0200

    he surely is... ;)

---
+++ lib/frontends/lish.py
@@ -0,0 +1,12 @@
+'''
+The Linspector Interactive Shell...
+
+This  will become an interface to Linspector at "start" time. Think about MidnightCommander... and then run
+Linspector in a screen session and not as daemon, why not? BTW.: Daemonization is at this point of development
+cancelled, because it makes no sense to daemonize everything. Linspector is a user software which will run in any
+screen session perfectly.
+'''
+
+class LishFrontend(Frontends):
+    def __init__(self, **kwargs):
+        return
