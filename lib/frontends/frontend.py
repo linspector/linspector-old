b092df118eed404dad46442754d9e1c8d28c2c3b
Johannes Findeisen <you@hanez.org>
2013-08-18 03:08:09 +0200

    just small typo fixes.

--- lib/frontends/frontend.py
+++ lib/frontends/frontend.py
@@ -3,7 +3,7 @@ Just the frontends.py stub... ;)
 
 If linspector is being started without a frontend "enabled" it just is doing stuff like: polling, alerting, logging etc.
 
-Frontends a absolutely no requirement for running Linspector.
+Frontends are absolutely no requirement for running Linspector.
 """
 
 
