a950e348c8b285a039b44205a92fa6bdf7795637
Johannes Findeisen <you@hanez.org>
2013-08-18 03:10:38 +0200

    just doc stuff

--- lib/frontends/lish.py
+++ lib/frontends/lish.py
@@ -1,10 +1,7 @@
 """
 Lish is the Linspector Interactive Shell...
 
-This  will become an interface to Linspector at "start" time. Think about MidnightCommander... and then run
-Linspector in a screen session and not as daemon, why not? BTW.: Daemonization is at this point of development
-cancelled, because it makes no sense to daemonize everything. Linspector is a user software which will run in any
-screen session perfectly.
+This  will become a commandline interface to Linspector. Think of a network switch or router like those from Cisco.
 """
 
 # what is this?
