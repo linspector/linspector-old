5f382da83b1cd67c2a3395ebbc4d8e0952d5efbb
Rafael.Timmerberg <raffn1@gmail.com>
2013-08-20 01:47:24 +0200

    added config

--- lib/tasks/email.py
+++ lib/tasks/email.py
@@ -4,21 +4,25 @@ The email task.
 http://docs.python.org/2/library/email-examples.html
 """
 
-import smtplib
-from email.mime.text import MIMEText
+#import smtplib
+#from email.mime.text import MIMEText
 from lib.tasks.task import Task
 
 
 class EmailTask(Task):
     def __init__(self, **kwargs):
+        pass
+        '''
         if not "type" in kwargs:
             raise Exception("'type' not in typeDict " + str(kwargs))
         if not "args" in kwargs:
             raise Exception("typeDict " + str(kwargs) + " has nor arguments!")
         self.set_task_type(kwargs["type"])
         self.recipient = kwargs["args"]["rcpt"]
+        '''
 
     def execute_task(self, msg):
+        '''
         message = MIMEText(msg)
         message['Subject'] = 'Warning from Linspector'
         message['From'] = "warning@linspector.org"
@@ -26,7 +30,8 @@ class EmailTask(Task):
         s = smtplib.SMTP('localhost')
         s.sendmail("warning@linspector.org", self.recipient, message.as_string())
         s.quit()
-
+        '''
+        pass
 
 def create(taskDict):
     return EmailTask(**taskDict)
