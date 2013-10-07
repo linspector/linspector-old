4571e52cde1e29e6600da8b519668f10c86b245d
Johannes Findeisen <you@hanez.org>
2013-10-08 00:08:47 +0200

    added license header to all code files

--- lib/backends/backend.py
+++ lib/backends/backend.py
@@ -3,6 +3,23 @@ Backends can be a http service or xml-rpc service; let's say background threads 
 should run as background threads.
 
 Backends are absolutely no requirement for running Linspector.
+
+Copyright (c) 2011-2013 "Johannes Findeisen and Rafael Timmerberg"
+
+This file is part of Linspector (http://linspector.org).
+
+Linspector is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 
--- lib/services/http.py
+++ lib/services/http.py
@@ -6,6 +6,23 @@ content could be fetched and compared.HTTPS is not validating the server certifi
 
 This should just return 0 on success and NOT 0 on error. Just to make internals generic
 to just report this code and not use a parser.
+
+Copyright (c) 2011-2013 "Johannes Findeisen and Rafael Timmerberg"
+
+This file is part of Linspector (http://linspector.org).
+
+Linspector is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import urllib
