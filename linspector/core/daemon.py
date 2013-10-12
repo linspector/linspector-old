e524bf3797f2378199682032e2c8172eac160061
Johannes Findeisen <you@hanez.org>
2013-10-12 02:34:39 +0200

    deleted the abstracted daemon and moved class over to daemon.py

--- linspector/core/daemon.py
+++ linspector/core/daemon.py
@@ -24,6 +24,27 @@ import time
 import atexit
 from signal import SIGTERM
 
+"""
+TODO: Think about, that daemonizing this software is not our goal but when we want to that, this needs a rewrite and
+should maybe move over to the daemon frontend. daemon.py will remain the the base for this and should stay in linspector/core .
+Since a daemon it normally not a frontend we should think about how to handle this. When daemonizing a real frontend
+like "Lish" will make no sense but a frontend like "https" or "xmpp" could be useful anyway...
+"""
+
+
+class LinspectorDaemon(Daemon):
+    def run(self):
+        while True:
+            try:
+                a = 2
+                #logger.writeLogToFile(_logfile, "Running!")
+                print "running!"
+            except Exception as err:
+                #logger.writeLogToFile(_logfile, str(err))
+                print "failed"
+                sys.exit(1)
+            time.sleep(1)
+
 
 class Daemon:
     """
