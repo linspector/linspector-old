41c882847d4f3e1217163ea28f6d7762a4deaa6a
Johannes Findeisen <you@hanez.org>
2013-11-05 01:19:56 +0100

    fixed interface bug after job.py refactoring

--- linspector/core/interface.py
+++ linspector/core/interface.py
@@ -55,12 +55,12 @@ class LinspectorInterface(object):
         d["Host"] = job.host
         d["Service"] = str(job.service)
         d["Members"] = str([member.name for member in job.members])
-        d["Period"] = str(job.job.trigger)
-        d["Next run"] = str(job.job.next_run_time)
-        d["Runs"] = str(job.job.runs)
-        d["Enabled"] = str(job._enabled)
+        d["Period"] = str(job.scheduler_job.trigger)
+        d["Next run"] = str(job.scheduler_job.next_run_time)
+        d["Runs"] = str(job.scheduler_job.runs)
+        d["Enabled"] = str(job.enabled)
         d["Threshold"] = str(job.service.get_threshold())
-        d["Fails"] = str(job.jobThreshold)
+        d["Fails"] = str(job.job_threshold)
         #d["Last Run"] = None
         #d["Last Fail"] = None
         #d["Last Success"] = None
